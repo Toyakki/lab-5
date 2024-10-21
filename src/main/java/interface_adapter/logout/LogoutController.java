c81516e4a9ead56fdc177e71b97db0027efabf62
Toyakki <totto1001@icloud.com>
2024-10-21 18:08:22 -0400

    The bug is fixed.

--- src/main/java/interface_adapter/logout/LogoutController.java
+++ src/main/java/interface_adapter/logout/LogoutController.java
@@ -24,7 +24,7 @@ public class LogoutController {
         // 1. instantiate the `LogoutInputData`, which should contain the username.
         // 2. tell the Interactor to execute.
         final LogoutInputData logoutInputData = new LogoutInputData(username);
-        logoutUseCaseInteractor.execute(logoutInputData);
+        this.logoutUseCaseInteractor.execute(logoutInputData);
 
     }
 }
